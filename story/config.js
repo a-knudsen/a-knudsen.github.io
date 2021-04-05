41b1a8b6ce38850725469972860b63b8d175d2dc
a-knudsen <ahnaknudsen@gmail.com>
2021-04-05 18:13:50 -0400

    Update config.js

--- story/config.js
+++ story/config.js
@@ -1,22 +1,22 @@
 var config = {
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/a-knudsen/ckn548ujk07au17oympomdrxz',
     accessToken: 'pk.eyJ1IjoiYS1rbnVkc2VuIiwiYSI6ImNrbjUwNHcxdTB6djkydXFzYXQ2dGpldGwifQ.h7Q9M54ubAgnBZ2OLCDBTQ',
     showMarkers: true,
     markerColor: '#3FB1CE',
     theme: 'light',
     use3dTerrain: false,
     title: 'REC Community Garden Network',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    subtitle: 'Explore Regional Environmental Council&#39; garden netowrk in Worcester, MA  ',
     byline: 'By Ahna Knudsen, 2021',
-    footer: 'Source: source citations, etc.',
+    footer: 'Source: Regional Environmental Council',
     chapters: [
         {
             id: 'id-one',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: 'Garden #1',
             image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'Insert Description here',
             location: {
                 center: { lon: -71.80640, lat: 42.25878 },
                 zoom: 13.40,
@@ -27,24 +27,24 @@ var config = {
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
+                 {
+                  layer: 'gardens-new-0o4tb6',
+                  opacity: 1
+                  duration: 5000
+                 }
             ],
             onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
+                 {
+                     layer: 'gardens-new-0o4tb6',
+                     opacity: 0
+                 }
             ]
         },
         {
             id: 'other-identifier-two',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
+            title: 'Garden #2',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
