e576456b9061127d6588371ab262c49b0fddc6fb
a-knudsen <ahnaknudsen@gmail.com>
2021-04-05 18:52:11 -0400

    Update config.js

--- story/config.js
+++ story/config.js
@@ -1,22 +1,22 @@
 var config = {
     style: 'mapbox://styles/a-knudsen/ckn548ujk07au17oympomdrxz',
     accessToken: 'pk.eyJ1IjoiYS1rbnVkc2VuIiwiYSI6ImNrbjUwNHcxdTB6djkydXFzYXQ2dGpldGwifQ.h7Q9M54ubAgnBZ2OLCDBTQ',
-    showMarkers: true,
+    showMarkers: false,
     markerColor: '#3FB1CE',
     theme: 'light',
     use3dTerrain: false,
     title: 'REC Community Garden Network',
-    subtitle: 'Explore Regional Environmental Council&#39; garden netowrk in Worcester, MA  ',
+    subtitle: 'Explore the garden network of Regional Environmental Council in Worcester, MA  ',
     byline: 'By Ahna Knudsen, 2021',
     footer: 'Source: Regional Environmental Council',
     chapters: [
         {
-            id: 'id-one',
+            id: 'ugrow',
             alignment: 'left',
             hidden: false,
             title: 'Urban Garden Resources of Worcester (UGROW)',
-            image: './path/to/image/source.png',
-            description: 'UGROW began in 1995 with 1 garden and 1 volunteer gardener with a vision to grow food in the city. Today, the network has grown to include over 60 community gardens throughout the city of Worcester involving more than 500 volunteer gardeners. Network gardens include neighborhood based gardens, school gardens and urban farms. UGROW poses an alternative to fossil-fueled, corporate agriculture and strengthens and beautifies our neighborhoods through promoting and supporting urban gardening.',
+            image: 'images/rec.jpg',
+            description: 'REC’s Urban Garden Resources of Worcester program (UGROW) coordinates a growing network of over 500 community gardeners at 60 urban community gardens, 20 of which are located within public schools. Together these gardens produce over 15,000 pounds of food annually.',
             location: {
                 center: { lon: -71.80640, lat: 42.25878 },
                 zoom: 13.40,
@@ -41,12 +41,12 @@ var config = {
             ]
         },
         {
-            id: 'other-identifier-two',
+            id: 'our-story',
             alignment: 'right',
             hidden: false,
-            title: 'Garden #2',
-            image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            title: 'Our Story',
+            image: './image/rec_ppl.jpg',
+            description: 'UGROW began in 1995 with 1 garden and 1 volunteer gardener with a vision to grow food in the city. Today, the network has grown to include over 60 community gardens throughout the city of Worcester involving more than 500 volunteer gardeners. Network gardens include neighborhood based gardens, school gardens and urban farms. UGROW poses an alternative to fossil-fueled, corporate agriculture and strengthens and beautifies our neighborhoods through promoting and supporting urban gardening.',
             location: {
                 center: { lon: -71.80640, lat: 42.25878 },
                 zoom: 13.40,
